b4388ce20e954dcdc243a69a9082a78e88285030
Shaanil <84518851+Shaanil@users.noreply.github.com>
2025-11-17 11:13:27 +0530

    Add files via upload

---
+++ _app.js
@@ -0,0 +1,7 @@
+// pages/_app.js
+import '../styles/globals.css'
+import { Analytics } from "@vercel/analytics/next"
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />
+}
